c0ad5ff0a46020bbaa5c578973a1d1bdcad83b55
wallycoding <wallysonbreno.dev@gmail.com>
2023-11-27 17:48:47 -0300

    test: testing RootLayout

---
+++ src/layout/RootLayout.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import { RootLayout } from './RootLayout'
+
+describe('RootLayout', () => {
+  const header = <h1>Header Example</h1>
+  const content = <div>Content Example</div>
+
+  beforeEach(() => {
+    render(<RootLayout header={header} children={content} />)
+  })
+
+  it('renders header props correctly', () => {
+    const header = screen.getByRole('heading', {
+      name: 'Header Example',
+    })
+    expect(header).toBeInTheDocument()
+  })
+
+  it('renders content props correctly', () => {
+    const content = screen.getByText('Content Example')
+    expect(content).toBeInTheDocument()
+  })
+})
