dc1af58fa8535583f52fe593cd2db8483b7515ea
wallycoding <wallysonbreno.dev@gmail.com>
2023-11-27 18:11:13 -0300

    feature: add component LeftSide

---
+++ src/components/app/components/Header/LeftSide.tsx
@@ -0,0 +1,12 @@
+export const LeftSide = () => {
+  return (
+    <div className="flex items-center gap-3">
+      <div className="min-w-[2.3rem]">
+        <img src="/logo.svg" alt="App Logo" />
+      </div>
+      <div>
+        <h1 className="font-medium text-capuccino-200">CoreNotes</h1>
+      </div>
+    </div>
+  );
+};
