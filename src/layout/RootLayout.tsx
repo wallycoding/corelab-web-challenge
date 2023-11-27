8ab0f74b1b5333a3a06e446f26537211b86f6d73
wallycoding <wallysonbreno.dev@gmail.com>
2023-11-27 17:52:35 -0300

    fix: prettier style code

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,13 +1,13 @@
 interface RootProps {
-  header: React.ReactNode
-  children: React.ReactNode
+  header: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const RootLayout = ({ header, children }: RootProps) => {
   return (
-    <div className="bg-capuccino-100 flex min-h-[100dvh] flex-col gap-10 pb-32">
+    <div className="flex min-h-[100dvh] flex-col gap-10 bg-capuccino-100 pb-32">
       <header className="flex">{header}</header>
       <main className="flex flex-col gap-10">{children}</main>
     </div>
-  )
-}
+  );
+};
